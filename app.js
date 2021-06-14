5b77e118f7f593f1ca9fa8a72bad009199f83aa7
Eva Chen <ec2604@nyu.edu>
2021-06-13 22:15:45 -0400

    First commit

---
+++ app.js
@@ -0,0 +1,31 @@
+//set initial count
+let count = 0;
+
+//select value and buttons
+const value = document.querySelector('#value');
+const btns = document.querySelectorAll('.btn');
+
+//access event object e, button in question directs counting up or down, set button color
+btns.forEach(function (btn) {
+  btn.addEventListener('click', function (e) {
+    const styles = e.currentTarget.classList;
+    if (styles.contains('decrease')) {
+      count--;
+    } else if (styles.contains('increase')) {
+      count++;
+    } else {
+      count = 0;
+    }
+    if (count > 0) {
+      value.style.color = 'green';
+    }
+    if (count < 0) {
+      value.style.color = 'red';
+    }
+    if (count === 0) {
+      value.style.color = '#222';
+    }
+    //sets span value to the running count
+    value.textContent = count;
+  });
+});
